a47d7b9b27f90b3463c1c7945705ea1bcb4965b2
frievoe97 <voelkers@campus.tu-berlin.de>
2020-10-17 01:58:50 +0200

    Reads the count data. And save the data in a list with alls station IDs. In each list element is a map with the months as the key and the counts per hour as the value.

--- src/main/java/org/matsim/prepare/CreateCityCounts.java
+++ src/main/java/org/matsim/prepare/CreateCityCounts.java
@@ -5,15 +5,20 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jfree.data.time.Day;
 import picocli.CommandLine;
+import scala.Int;
+import scala.util.parsing.combinator.testing.Str;
 
+import javax.lang.model.util.Types;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
@@ -45,11 +50,20 @@ public class CreateCityCounts implements Callable<Integer> {
                 .map(this::readCounts)
                 .collect(Collectors.toList());
 
-        // list of months, mapped to station mapped to day
+        for (Map<String, Map<String, DayCounts>> collectTmp : collect) {
+            for (Map.Entry<String, Map<String, DayCounts>> collect2Tmp : collectTmp.entrySet()) {
 
-        // TODO: transform and aggregate
-        // station id as top level
+                log.info("**********************************************************************************************************************************");
+                log.info("* Stattion ID: " + collect2Tmp.getKey());
 
+                for (Map.Entry<String, DayCounts> collect3Tmp : collect2Tmp.getValue().entrySet()) {
+
+                    log.info("* Month: " + collect3Tmp.getKey());
+                    log.info("* Counts per hour: " + collect3Tmp.getValue().getCounts());
+                }
+                log.info("**********************************************************************************************************************************");
+            }
+        }
         return 0;
     }
 
@@ -67,19 +81,17 @@ public class CreateCityCounts implements Callable<Integer> {
                 if (entry.isDirectory())
                     continue;
 
-                Map<String, DayCounts> month = readCsvCounts(in);
+                String id = entry.getName().split("_")[2].substring(0, 15);
+                String monthNumber = entry.getName().split("Verarbeitet-19")[1].split("01_")[0];
 
-                // TODO which month is contained in the zip file name
-                result.put(entry.getName(), month);
+                Map<String, DayCounts> month = readCsvCounts(in, monthNumber);
 
+                result.put(id, month);
                 log.info("Finished reading {}", entry.getName());
             }
-
         } catch (IOException e) {
             log.error("Could not read zip file {}", zip, e);
         }
-
-
         return result;
     }
 
@@ -88,24 +100,49 @@ public class CreateCityCounts implements Callable<Integer> {
      *
      * @return map of date to counts for whole day.
      */
-    private Map<String, DayCounts> readCsvCounts(ZipInputStream in) throws IOException {
+    private Map<String, DayCounts> readCsvCounts(ZipInputStream in, String monthNumber) throws IOException {
 
-        // reader must not be closed
+        Map<Integer, List<Double>> tempCountSum = new HashMap<>();
+        Map<String, DayCounts> result = new HashMap<>();
+        List<String> holidays2019 = Arrays.asList("01.01.2019", "19.04.2019", "22.04.2019", "01.05.2019",
+                "30.05.2019", "10.06.2019", "20.06.2019", "03.10.2019", "01.11.2019", "25.12.2019",
+                "26.12.2019");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        List<Integer> weekendDaysList = Arrays.asList(1, 5, 6, 7);
+        Integer[] hourCountsTmp = new Integer[24];
+        Double countMean;
+
         InputStreamReader reader = new InputStreamReader(in);
+
         CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT
                 .withDelimiter(';')
                 .withFirstRecordAsHeader()
         );
 
-        Map<String, DayCounts> result = new HashMap<>();
-
         for (CSVRecord record : parser) {
+            if (!isWeekend(LocalDate.parse(record.get("Time").split(" ")[0], formatter), weekendDaysList) || !holidays2019.contains(record.get("Time").split(" ")[0])) {
 
-            // TODO: aggregate by hours
-//                System.out.println(record);
+                Integer hour = Integer.parseInt(record.get("Time").split(" ")[1].split(":")[0]);
+                Double value = Double.valueOf(record.get("processed_all_vol").replaceAll(",", "."));
+                tempCountSum.computeIfAbsent(hour, k -> new ArrayList<>()).add(value);
+            }
         }
 
+        for (Map.Entry<Integer, List<Double>> meanCounts : tempCountSum.entrySet()) {
+            countMean = 0.0;
+            for (Double value : meanCounts.getValue()) {
+                countMean += value;
+            }
+            countMean = countMean/(meanCounts.getValue().size());
+            hourCountsTmp[meanCounts.getKey()] = countMean.intValue();
+        }
+        DayCounts dayCount = new DayCounts(hourCountsTmp);
+        result.put(monthNumber, dayCount);
         return result;
+    }
+
+    private boolean isWeekend(LocalDate date, List<Integer> weekendDaysList) {
+        return weekendDaysList.contains(date.getDayOfWeek().getValue());
     }
 
 
@@ -117,8 +154,20 @@ public class CreateCityCounts implements Callable<Integer> {
         /**
          * Counts for each hour of the day.
          */
-        double[] counts = new double[24];
+        Integer[] counts;
 
+        public DayCounts(Integer[] counts) {
+            this.counts = counts;
+        }
+
+        public String getCounts() {
+            String ret = "[";
+            for (Integer count : counts) {
+                ret = ret + count.toString() + ", ";
+            }
+            ret = ret.substring(0, ret.length() - 2);
+            ret = ret + "]";
+            return ret;
+        }
     }
-
 }
