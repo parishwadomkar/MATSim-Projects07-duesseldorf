be8f13a4dcc8fc26769c3df2c279286873b9d34d
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-03-29 13:51:32 +0200

    Update AddingFreightPersonsToPopulationFile.java

--- src/main/java/org/matsim/freight/AddingFreightPersonsToPopulationFile.java
+++ src/main/java/org/matsim/freight/AddingFreightPersonsToPopulationFile.java
@@ -7,26 +7,44 @@ import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
+import picocli.CommandLine;
 
-public class AddingFreightPersonsToPopulationFile {
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+		name = "add-freight-trips-tp-population",
+		description = "Add freight trips to original population",
+		showDefaultValues = true
+)
+
+public class AddingFreightPersonsToPopulationFile implements Callable<Integer> {
 	// In this part, the freight only plan will be merged with the population plan
-	private static final String POPULATIAON_FILE = "C:\\Users\\cluac\\MATSimScenarios\\"
-			+ "Dusseldorf\\Scenario\\duesseldorf-v1.2-10pct.plans.xml.gz";
-	private static final String FREIGHT_ONLY_FILE = "C:\\Users\\cluac\\MATSimScenarios\\"
-			+ "Dusseldorf\\freight\\10pct-freight-only-plans.xml";
-	private static final String OUTPUT_PATH = "C:\\Users\\cluac\\MATSimScenarios\\"
-			+ "Dusseldorf\\Scenario\\duesseldorf-v1.3-10pct.plans.xml.gz";
+	// Coordinate System transformation will be performed in this step.
+	@CommandLine.Option(names = "--input", description = "Path to the original population", required = true)
+	private Path inputPopulation;
+
+	@CommandLine.Option(names = "--freight", description = "Path to the freight only plans", required = true)
+	private Path freightOnlyPlans;
+
+	@CommandLine.Option(names = "--output", description = "Output path", required = true)
+	private Path outputPath;
 
 	public static void main(String[] args) {
+		System.exit(new CommandLine(new AddingFreightPersonsToPopulationFile()).execute(args));
+	}
+
+	@Override
+	public Integer call() throws Exception {
 		Config config = ConfigUtils.createConfig();
 		config.global().setCoordinateSystem("EPSG:25832");
-		config.plans().setInputFile(POPULATIAON_FILE);
+		config.plans().setInputFile(inputPopulation.toString());
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		Population population = scenario.getPopulation();
 
 		Config freightConfig = ConfigUtils.createConfig();
 		freightConfig.global().setCoordinateSystem("EPSG:25832");
-		freightConfig.plans().setInputFile(FREIGHT_ONLY_FILE);
+		freightConfig.plans().setInputFile(freightOnlyPlans.toString());
 		freightConfig.plans().setInputCRS("EPSG:5677");
 		Scenario freightScenario = ScenarioUtils.loadScenario(freightConfig);
 		Population freightOnlyPlans = freightScenario.getPopulation();
@@ -39,7 +57,8 @@ public class AddingFreightPersonsToPopulationFile {
 		// Write population
 		System.out.println("Writing population file...");
 		PopulationWriter pw = new PopulationWriter(population);
-		pw.write(OUTPUT_PATH);
+		pw.write(outputPath.toString());
 
+		return 0;
 	}
 }
