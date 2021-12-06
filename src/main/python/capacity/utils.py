8fafa972b9242877d393f9e956242d05fb8c5342
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-12-06 09:02:20 +0100

    added script for junction volumes

--- src/main/python/capacity/utils.py
+++ src/main/python/capacity/utils.py
@@ -2,10 +2,36 @@
 
 import os
 import sys
-import json
+import argparse
 
 from subprocess import call
 
+
+def create_args(description):
+    import sumolib
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("input", nargs=1, help="Path to input csv")
+
+    parser.add_argument("--output", default="output", help="Path to output folder")
+    parser.add_argument("--network", type=str, default="../../../../scenarios/input/sumo.net.xml", help="Path to network file")
+    parser.add_argument("--veh", type=int, default=5000, help="Vehicles per hour per lane simulate")
+    parser.add_argument("--cv", type=float, default=1, help="Share of conventional vehicles")
+    parser.add_argument("--av", type=float, default=0, help="Share of automated vehicles")
+    parser.add_argument("--acv", type=float, default=0, help="Share of connected autonomous vehicles")
+    parser.add_argument("--from-index", type=int, default=0, help="Start from number")
+    parser.add_argument("--to-index", type=int, default=-1, help="Stop at number")
+    parser.add_argument("--step-length", type=float, default=0.2, help="SUMO step length")
+    parser.add_argument("--runner", type=str, default="runner0", help="Runner id")
+
+    args = parser.parse_args()
+    args.port = sumolib.miscutils.getFreeSocketPort()
+
+    os.makedirs(args.output, exist_ok=True)
+    os.makedirs(args.runner, exist_ok=True)
+
+    return args
 
 def init_env():
     if 'SUMO_HOME' in os.environ:
@@ -15,7 +41,7 @@ def init_env():
         sys.exit("please declare environment variable 'SUMO_HOME'")
 
 
-def write_scenario(f, network_file, route_file, additional_file, step_length=0.2):
+def write_scenario(f, network_file, route_file, additional_file, step_length=0.2, time=600):
     """ Write sumo scenario file """
 
     with open(f, "w") as fn:
@@ -30,36 +56,30 @@ def write_scenario(f, network_file, route_file, additional_file, step_length=0.2
 
     <time>
         <begin value="0"/>
-        <end value="600"/>
+        <end value="%d"/>
         <step-length value="%f"/>
     </time>
 </configuration>
-""" % (network_file, route_file, additional_file, step_length))
+""" % (network_file, route_file, additional_file, time, step_length))
 
 
-def filter_network(netconvert, netfile, edge, output):
+def filter_network(netconvert, netfile, edge, output, args=None):
 
-    x = [s[0] for s in edge.getShape()]
-    y = [s[1] for s in edge.getShape()]
+    if isinstance(edge, list):
+        x = [s[0] for e in edge for s in e.getShape()]
+        y = [s[1] for e in edge for s in e.getShape()]
+    else:
+        x = [s[0] for s in edge.getShape()]
+        y = [s[1] for s in edge.getShape()]
 
     # minX,minY,maxX,maxY
     boundary = ",".join(str(s) for s in [min(x) - 50, min(y) - 50, max(x) + 50, max(y) + 50])
 
-    call([netconvert, '-s', netfile, "--keep-edges.in-boundary", boundary, '-o', output])
+    cmd = [netconvert, '-s', netfile, "--keep-edges.in-boundary", boundary]
 
-def saveToFile(data, filename):
-    json.dump(data, open(filename, "w"))
+    if args:
+        cmd += args
 
+    cmd += ['-o', output]
 
-def readFromFile(filename):
-    if os.path.exists(filename):
-        pass
-    else:
-        saveToFile([], filename)
-    return (json.load(open(filename)))
-
-
-def writeDone(data):
-    temp = readFromFile("done.json")
-    temp.append(data)
-    saveToFile(temp, "done.json")
+    call(cmd)
