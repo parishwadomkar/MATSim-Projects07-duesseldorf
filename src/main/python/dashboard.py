2bc6be4067d39d0b99a23ff384aa609168b01567
Christian Rakow <rakow@vsp.tu-berlin.de>
2022-02-07 17:08:56 +0100

    added more plots to first dashboard

--- src/main/python/dashboard.py
+++ src/main/python/dashboard.py
@@ -1,6 +1,7 @@
 
 import os
 from os.path import join
+from glob import glob
 
 import pandas as pd
 import geopandas as gpd
@@ -50,7 +51,7 @@ gdf = gpd.sjoin(gdf, shp, how="inner", op="intersects").to_crs("EPSG:4326")
 #%%
 
 # Only use relevant columns
-json = gdf[["wkt"]].to_json()
+json = gdf[["wkt"]].reset_index().to_json()
 
 with open(join(out, "network.geojson"), "w") as f:
     f.write(json)
@@ -61,28 +62,58 @@ df = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/base/linkStats.csv.gz"
 
 #%%
 
-def f(col):
-    return ";".join(str(x) for x in col)
 
-link_volumes = df.groupby("linkId").agg(vol=("vol_car", f))
+link_volumes = df.groupby("linkId").agg(volume=("vol_car", sum))
 link_volumes = gdf.merge(link_volumes, left_on="link_id", right_index=True)
 
 with open(join(out, "link_volumes.csv"), "w") as f:
     
-    f.write("link;")    
-    f.write(";".join("%02d:00" % h for h in range(24)))
-    f.write("\n")
-    
+    f.write("link;volume\n")
     for row in link_volumes.itertuples():
         
         f.write(row.Index)
         f.write(";")
-        f.write(row.vol)
+        f.write(str(row.volume))
         f.write("\n")
         
+#%%
+
+iters = [d for d in os.scandir("Z:/matsim-duesseldorf/experiment/output/base/ITERS/") if d.is_dir()]
+iters = sorted(iters, key=lambda d: d.stat().st_atime)
+
+hist = glob(iters[-1].path + "/*.legHistogram.txt")[0]
+
+df = pd.read_csv(hist, delimiter="\t", index_col=0)
+
+df = df.rename(columns={"en-route_car": "Car"})[["Car"]]
+
+# Only use 24 hours
+df.iloc[0:(24*12)].to_csv(join(out, "base_car_traffic.csv"))
 
 
 #%%
+
+from matsim.calibration import calc_mode_share
+
+def pf(persons):
+    df = gpd.sjoin(persons.set_crs("EPSG:25832"), city, how="inner", op="intersects")
+    return df
+
+def mt(df):
+
+    # Car and ride are merged in the survey data
+    df.loc[df.main_mode == "ride", "main_mode"] = "car"
+    return df[df.main_mode != "freight"]
+
+mode_share = calc_mode_share("Z:/matsim-duesseldorf/experiment/output/base", person_filter=pf, map_trips=mt)
+
+#%%
+
+with open(join(out, "base_modeshare.csv"), "w") as f:
+
+    f.write(",".join(mode_share.index))
+    f.write("\n")    
+    f.write(",".join(str(s) for s in mode_share))
 
 
 
